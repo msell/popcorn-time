0c26a704f31cfb66e80ab6ea81b8900b40eb6c28
Matt Sell <mwsell@gmail.com>
2015-06-10 21:34:23 -0500

    converting first video

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-
-
-
-var FfmpegCommand = require('fluent-ffmpeg');
+var child_process = require('child_process');
 var argv = require('yargs').argv;
-var cmd = new FfmpegCommand();
 var fs = require('fs');
 var path = require('path');
 var dammit = require('dammit');
 var chalk = require('chalk');
 var filteredFiles = require('./filteredFiles.js');
-  
-if(!argv.source){
-    goodbye('I need you to at least specify the --source');
+
+if (!argv.source) {
+  goodbye('I need you to at least specify the --source');
 }
 
 var source = argv.source;
 
-if(!fs.existsSync(source)){
-    goodbye(source + ' does not exist');
+if (!fs.existsSync(source)) {
+  goodbye(source + ' does not exist');
 }
 
-filteredFiles(source, console.log);
+filteredFiles(source, convert);
 
-function goodbye(reason){
-    console.log(chalk.red(dammit({'NSFW': true}))); 
-    console.log(chalk.red(reason));
-    process.exit();
+function convert(files) {
+  child_process.exec('ffmpeg -i ' + files[0] + ' -c:v copy -c:a libfaac -ac 6 -ar 48000 -ab 448k /Users/matt/Downloads/temp.mp4', function (err, stdout, stderr) {
+    if (err) throw err;
+
+    console.log(stdout);
+
+  });
+}
+
+function goodbye(reason) {
+  console.log(chalk.red(dammit({
+    'NSFW': true
+  })));
+  console.log(chalk.red(reason));
+  process.exit();
 }
 
 
 // ffmpeg -i HomeMovie.mkv -c:v copy -c:a libfaac -ac 6 -ar 48000 -ab 448k HomeMovie.mp4
-
